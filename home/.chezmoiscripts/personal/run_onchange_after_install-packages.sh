98b4c2f9ed28cc564db9817cbc9c15890fd9c33b
sruddle <sarahruddle1@gmail.com>
2026-05-04 02:42:34 +0200

    Update run_onchange_after_install-packages.sh
    add casks and fence rclone install

--- home/.chezmoiscripts/personal/run_onchange_after_install-packages.sh
+++ home/.chezmoiscripts/personal/run_onchange_after_install-packages.sh
@@ -21,11 +21,14 @@ brew install --cask chromedriver
 brew install --cask firefox
 brew install --cask font-meslo-lg-nerd-font
 brew install --cask xquartz
-# brew install --cask iterm2
-# brew install --cask notion
-# brew install --cask visual-studio-code
+brew install --cask iterm2
+brew install --cask notion
+brew install --cask visual-studio-code
 
-curl https://rclone.org/install.sh | bash
+if ! is_installed rclone; then
+    echo "rclone not found. Installing."
+    curl https://rclone.org/install.sh | bash
+fi
 
 if ! is_installed /opt/homebrew/bin/mise; then
     echo "mise not found. Aborting."
